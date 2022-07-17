7baba67d2801a8504ffc9b058a5a1e61d7b575e1
Ainur <ainurforex@mail.ru>
2022-07-17 23:00:32 +0500

    2 тип добавил

--- src/main/java/ru/ainurforex/cellwar/CellMachine.java
+++ src/main/java/ru/ainurforex/cellwar/CellMachine.java
@@ -1,6 +1,5 @@
 package ru.ainurforex.cellwar;
 
-import javafx.event.ActionEvent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
@@ -9,6 +8,8 @@ public class CellMachine {
     private static int width = 1000;
     private static int height = 1000;
     private static int z = 0;
+    private static int teamRed=0;
+    private static int teamBlue=0;
     public static int[][][] cells = new int[width][height][2];
 
 
@@ -23,14 +24,19 @@ public class CellMachine {
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
                 if (CellMachine.cells[i][j][z] == 1) {
-                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.CORAL);
-                } else {
+                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.RED);
+                }
+                if (CellMachine.cells[i][j][z] == 2) {
+                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.BLUE);
+                }
+                if (CellMachine.cells[i][j][z] == 0) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.WHITE);
                 }
+
             }
+
+
         }
-
-
     }
 
     public void drawCellMouse(int mouseX, int mouseY) {
@@ -47,11 +53,19 @@ public class CellMachine {
     public void randomCells() {
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
-                if ((int) (Math.random() * 5) == 1) {
-                    CellMachine.cells[i][j][z] = 1;
-                } else {
-                    CellMachine.cells[i][j][z] = 0;
+                int rand = (int) (Math.random() * 5);
+                switch (rand) {
+                    case (1):
+                        CellMachine.cells[i][j][z] = 1;
+                        break;
+                    case (2):
+                        CellMachine.cells[i][j][z] = 2;
+                        break;
+                    default:
+                        CellMachine.cells[i][j][z] = 0;
+                        break;
                 }
+
             }
         }
 
@@ -67,17 +81,37 @@ public class CellMachine {
 
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
-              /*  if (cells[i][j][z] == 0 && summ8cells(i, j) == 3) {
+                int invasionKoef1 = 3;
+                int invasionKoef2 = 3;
+
+                int summType1 = summ8cellsType(i, j, 1);
+                int summType2 = summ8cellsType(i, j, 2);
+
+                if (cells[i][j][z] == 0 && summType1 == 3 && summType2 < invasionKoef1) {
                     cells[i][j][z1] = 1;
                 }
-                if (cells[i][j][z] == 1 && summ8cells(i, j) < 2|| summ8cells(i, j) > 3) {
-                    cells[i][j][z1] = 0;
-                }
-*/
-               if ((cells[i][j][z] == 1 && summ8cells(i, j) == 2)||summ8cells(i, j) == 3) {
-                    cells[i][j][z1] = 1;
+                if (cells[i][j][z] == 0 && summType1 < invasionKoef2 && summType2 == 3) {
+                    cells[i][j][z1] = 2;
                 }
 
+
+                if ((cells[i][j][z] == 1 && summType2 < invasionKoef1) && (summType1 == 2 || summType1 == 3)) {
+                    cells[i][j][z1] = 1;
+                }
+                if ((cells[i][j][z] == 2 && summType1 < invasionKoef2) && (summType2 == 2 || summType2 == 3)) {
+                    cells[i][j][z1] = 2;
+                }
+
+                if (cells[i][j][z] == 1 && (summType1 < 2 || summType1 > 3|| summType1+summType2 >3)) {
+                    cells[i][j][z1] = 0;
+                }
+                if (cells[i][j][z] == 2  && (summType2 < 2 || summType2 > 3|| summType1+summType2 >3)) {
+                    cells[i][j][z1] = 0;
+                }
+                /*if ((cells[i][j][z] == 1 && summ8cells(i, j) == 2)||summ8cells(i, j) == 3) {
+                   cells[i][j][z1] = 1;
+                 }
+                */
             }
 
 
@@ -93,21 +127,93 @@ public class CellMachine {
     public static int summ8cells(int x, int y) {
         int summ = 0;
 
-        summ = cells[x - 1][y - 1][z] + cells[x][y - 1][z] + cells[x + 1][y - 1][z] +
-                cells[x - 1][y][z] + cells[x + 1][y][z] +
-                cells[x - 1][y + 1][z] + cells[x][y + 1][z] + cells[x + 1][y + 1][z];
+        if (cells[x - 1][y - 1][z] > 0) {
+            summ++;
+        }
+        if (cells[x][y - 1][z] > 0) {
+            summ++;
+        }
+        if (cells[x + 1][y - 1][z] > 0) {
+            summ++;
+        }
+        if (cells[x - 1][y][z] > 0) {
+            summ++;
+        }
+        if (cells[x + 1][y][z] > 0) {
+            summ++;
+        }
+        if (cells[x - 1][y + 1][z] > 0) {
+            summ++;
+        }
+        if (cells[x][y + 1][z] > 0) {
+            summ++;
+        }
+        if (cells[x + 1][y + 1][z] > 0) {
+            summ++;
+        }
+
+        return summ;
+          /*  summ = cells[x - 1][y - 1][z] + cells[x][y - 1][z] + cells[x + 1][y - 1][z] +
+                    cells[x - 1][y][z] + cells[x + 1][y][z] +
+                    cells[x - 1][y + 1][z] + cells[x][y + 1][z] + cells[x + 1][y + 1][z];
+        */
+
+    }
+
+    public static int summ8cellsType(int x, int y, int type) {
+        int summ = 0;
+        if (cells[x - 1][y - 1][z] == type) {
+            summ++;
+        }
+        if (cells[x][y - 1][z] == type) {
+            summ++;
+        }
+        if (cells[x + 1][y - 1][z] == type) {
+            summ++;
+        }
+        if (cells[x - 1][y][z] == type) {
+            summ++;
+        }
+        if (cells[x + 1][y][z] == type) {
+            summ++;
+        }
+        if (cells[x - 1][y + 1][z] == type) {
+            summ++;
+        }
+        if (cells[x][y + 1][z] == type) {
+            summ++;
+        }
+        if (cells[x + 1][y + 1][z] == type) {
+            summ++;
+        }
 
         return summ;
 
     }
 
     private static void clearArray() {
+         teamBlue = 0;
+         teamRed = 0;
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
+                if (cells[i][j][z] == 1) {
+                    teamRed++;
+                }
+                if (cells[i][j][z] == 2) {
+                    teamBlue++;
+                }
+
                 cells[i][j][z] = 0;
             }
         }
+
     }
 
+    public static int getTeamRed() {
+        return teamRed;
+    }
 
+    public static int getTeamBlue() {
+        return teamBlue;
+    }
 }
--- src/main/java/ru/ainurforex/cellwar/CellWarController.java
+++ src/main/java/ru/ainurforex/cellwar/CellWarController.java
@@ -2,53 +2,62 @@ package ru.ainurforex.cellwar;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.canvas.Canvas;
 
 public class CellWarController {
+    @FXML
+    public TextField teamRed;
+    @FXML
+    public TextField teamBlue;
     private static int mouseX;
     private static int mouseY;
     public Canvas canvas;
 
 
     public void updateMouse(MouseEvent e) {
-        CellMachine draw=new CellMachine(canvas);
+        CellMachine draw = new CellMachine(canvas);
         mouseX = (int) e.getSceneX();
         mouseY = (int) e.getSceneY();
-        draw.drawCellMouse(mouseX,mouseY);
+        draw.drawCellMouse(mouseX, mouseY);
     }
 
 
-private void stepArray() {
+    private void stepArray() {
+        CellMachine draw = new CellMachine(canvas);
+        CellMachine.cellsCalculate();
+        teamBlue.setText(CellMachine.getTeamBlue() + "");
+        teamRed.setText(CellMachine.getTeamRed() + "");
 
-    CellMachine draw=new CellMachine(canvas);
-    CellMachine.cellsCalculate();
-    draw.drawArray();
-}
+        draw.drawArray();
+    }
 
     private void stepUntil() {
-        for (int i = 0;; i++) {
-
+        for (int i = 0; i < 10; i++) {
             stepArray();
-        }
 
+        }
     }
 
     @FXML
     private void buttonStep1(ActionEvent actionEvent) {
-       stepArray();
+        stepArray();
     }
 
     @FXML
     private void buttonStepUntil(ActionEvent actionEvent) {
-        Thread s = new Thread(() -> stepUntil());
-        s.start();
+        // Thread s = new Thread(() -> stepUntil());
+        //s.start();
+        stepUntil();
     }
+
     @FXML
     private void randomCells(ActionEvent actionEvent) {
-        CellMachine draw=new CellMachine(canvas);
-         draw.randomCells();
+        CellMachine draw = new CellMachine(canvas);
+        draw.randomCells();
         draw.drawArray();
     }
+
 
 }
