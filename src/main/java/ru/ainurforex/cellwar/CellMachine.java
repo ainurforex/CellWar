d8ad84d3d62cabba633502dd549aa315f492ee4a
Ainur <ainurforex@mail.ru>
2022-07-18 10:18:45 +0500

    обавил поля вокруг и спавн по кнопке

--- src/main/java/ru/ainurforex/cellwar/CellMachine.java
+++ src/main/java/ru/ainurforex/cellwar/CellMachine.java
@@ -8,8 +8,8 @@ public class CellMachine {
     private static int width = 1000;
     private static int height = 1000;
     private static int z = 0;
-    private static int teamRed=0;
-    private static int teamBlue=0;
+    private static int teamRed = 0;
+    private static int teamBlue = 0;
     public static int[][][] cells = new int[width][height][2];
 
 
@@ -23,15 +23,23 @@ public class CellMachine {
 
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
-                if (CellMachine.cells[i][j][z] == 1) {
+                int cellCentre = CellMachine.cells[i][j][z];
+                if (cellCentre == 1) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.RED);
                 }
-                if (CellMachine.cells[i][j][z] == 2) {
+                if (cellCentre == 2) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.BLUE);
                 }
-                if (CellMachine.cells[i][j][z] == 0) {
+                if (cellCentre == 0) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.WHITE);
+                    if (summ8cellsType(i, j, 1) > 0 && summ8cellsType(i, j, 2) == 0) {
+                        canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.GOLD);
+                    }
+                    if (summ8cellsType(i, j, 2) > 0 && summ8cellsType(i, j, 1) == 0) {
+                        canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.TURQUOISE);
+                    }
                 }
+
 
             }
 
@@ -39,27 +47,36 @@ public class CellMachine {
         }
     }
 
+
     public void drawCellMouse(int mouseX, int mouseY) {
-        canvas.getGraphicsContext2D().getPixelWriter().setColor(mouseX - 15, mouseY - 15, Color.CORAL);
-        CellMachine.arrayWright(mouseX - 15, mouseY - 15);
+        int type=((int) (Math.random() * 2))+1;
+        CellMachine.arrayWright(mouseX - 15, mouseY - 15,type);
+        if (type == 1) {
+            canvas.getGraphicsContext2D().getPixelWriter().setColor(mouseX - 15, mouseY - 15, Color.RED);
+        }
+        if (type == 2) {
+            canvas.getGraphicsContext2D().getPixelWriter().setColor(mouseX - 15, mouseY - 15, Color.BLUE);
+        }
 
     }
 
 
-    public static void arrayWright(int x, int y) {
-        cells[x][y][z] = 1;
+    public static void arrayWright(int x, int y,int type) {
+        cells[x][y][z] = type;
     }
 
     public void randomCells() {
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
-                int rand = (int) (Math.random() * 5);
+                int rand = (int) (Math.random() * 20);
                 switch (rand) {
                     case (1):
                         CellMachine.cells[i][j][z] = 1;
+                        teamRed++;
                         break;
                     case (2):
                         CellMachine.cells[i][j][z] = 2;
+                        teamBlue++;
                         break;
                     default:
                         CellMachine.cells[i][j][z] = 0;
@@ -70,6 +87,29 @@ public class CellMachine {
         }
 
     }
+
+    public void putRandomTypeCells(int iteration,int type) {
+
+
+        for (int i = 0; i < iteration; i++) {
+            int randX = (int) (Math.random() * 998) + 1;
+            int randY = (int) (Math.random() * 998) + 1;
+            if (summ8cellsType(randX, randY, type) > 0) {
+                CellMachine.cells[randX][randY][z] = type;
+                if (type == 1) {
+                    teamRed++;
+                }
+                if (type == 2) {
+                    teamBlue++;
+                }
+            }
+
+        }
+
+
+
+    }
+
 
     public static void cellsCalculate() {
         int z1 = 0;
@@ -83,29 +123,30 @@ public class CellMachine {
             for (int j = 1; j < 1000 - 1; j++) {
                 int invasionKoef1 = 3;
                 int invasionKoef2 = 3;
+                int cellCentre = cells[i][j][z];
 
                 int summType1 = summ8cellsType(i, j, 1);
                 int summType2 = summ8cellsType(i, j, 2);
 
-                if (cells[i][j][z] == 0 && summType1 == 3 && summType2 < invasionKoef1) {
+                if (cellCentre == 0 && summType1 == 3 && summType2 < invasionKoef1) {
                     cells[i][j][z1] = 1;
                 }
-                if (cells[i][j][z] == 0 && summType1 < invasionKoef2 && summType2 == 3) {
+                if (cellCentre == 0 && summType1 < invasionKoef2 && summType2 == 3) {
                     cells[i][j][z1] = 2;
                 }
 
 
-                if ((cells[i][j][z] == 1 && summType2 < invasionKoef1) && (summType1 == 2 || summType1 == 3)) {
+                if ((cellCentre == 1 && summType2 < invasionKoef1) && (summType1 == 2 || summType1 == 3)) {
                     cells[i][j][z1] = 1;
                 }
-                if ((cells[i][j][z] == 2 && summType1 < invasionKoef2) && (summType2 == 2 || summType2 == 3)) {
+                if ((cellCentre == 2 && summType1 < invasionKoef2) && (summType2 == 2 || summType2 == 3)) {
                     cells[i][j][z1] = 2;
                 }
 
-                if (cells[i][j][z] == 1 && (summType1 < 2 || summType1 > 3|| summType1+summType2 >3)) {
+                if (cellCentre == 1 && (summType1 < 2 || summType1 > 3 || summType1 + summType2 > 3)) {
                     cells[i][j][z1] = 0;
                 }
-                if (cells[i][j][z] == 2  && (summType2 < 2 || summType2 > 3|| summType1+summType2 >3)) {
+                if (cellCentre == 2 && (summType2 < 2 || summType2 > 3 || summType1 + summType2 > 3)) {
                     cells[i][j][z1] = 0;
                 }
                 /*if ((cells[i][j][z] == 1 && summ8cells(i, j) == 2)||summ8cells(i, j) == 3) {
@@ -124,41 +165,7 @@ public class CellMachine {
         }
     }
 
-    public static int summ8cells(int x, int y) {
-        int summ = 0;
 
-        if (cells[x - 1][y - 1][z] > 0) {
-            summ++;
-        }
-        if (cells[x][y - 1][z] > 0) {
-            summ++;
-        }
-        if (cells[x + 1][y - 1][z] > 0) {
-            summ++;
-        }
-        if (cells[x - 1][y][z] > 0) {
-            summ++;
-        }
-        if (cells[x + 1][y][z] > 0) {
-            summ++;
-        }
-        if (cells[x - 1][y + 1][z] > 0) {
-            summ++;
-        }
-        if (cells[x][y + 1][z] > 0) {
-            summ++;
-        }
-        if (cells[x + 1][y + 1][z] > 0) {
-            summ++;
-        }
-
-        return summ;
-          /*  summ = cells[x - 1][y - 1][z] + cells[x][y - 1][z] + cells[x + 1][y - 1][z] +
-                    cells[x - 1][y][z] + cells[x + 1][y][z] +
-                    cells[x - 1][y + 1][z] + cells[x][y + 1][z] + cells[x + 1][y + 1][z];
-        */
-
-    }
 
     public static int summ8cellsType(int x, int y, int type) {
         int summ = 0;
@@ -192,8 +199,8 @@ public class CellMachine {
     }
 
     private static void clearArray() {
-         teamBlue = 0;
-         teamRed = 0;
+        teamBlue = 0;
+        teamRed = 0;
         for (int i = 1; i < 1000 - 1; i++) {
             for (int j = 1; j < 1000 - 1; j++) {
                 if (cells[i][j][z] == 1) {
--- src/main/java/ru/ainurforex/cellwar/CellWarController.java
+++ src/main/java/ru/ainurforex/cellwar/CellWarController.java
@@ -29,15 +29,15 @@ public class CellWarController {
         CellMachine.cellsCalculate();
         teamBlue.setText(CellMachine.getTeamBlue() + "");
         teamRed.setText(CellMachine.getTeamRed() + "");
-
         draw.drawArray();
+
     }
 
     private void stepUntil() {
-        for (int i = 0; i < 10; i++) {
-            stepArray();
-
+        for (int i = 0; i < 100; i++) {
+            CellMachine.cellsCalculate();
         }
+        stepArray();
     }
 
     @FXML
@@ -46,9 +46,8 @@ public class CellWarController {
     }
 
     @FXML
-    private void buttonStepUntil(ActionEvent actionEvent) {
-        // Thread s = new Thread(() -> stepUntil());
-        //s.start();
+    private void buttonStep100(ActionEvent actionEvent) {
+
         stepUntil();
     }
 
@@ -57,6 +56,24 @@ public class CellWarController {
         CellMachine draw = new CellMachine(canvas);
         draw.randomCells();
         draw.drawArray();
+        teamBlue.setText(CellMachine.getTeamBlue() + "");
+        teamRed.setText(CellMachine.getTeamRed() + "");
+    }
+
+    @FXML
+    private void add1000Red(ActionEvent actionEvent) {
+        CellMachine draw = new CellMachine(canvas);
+        draw.putRandomTypeCells(1000, 1);
+        draw.drawArray();
+        teamRed.setText(CellMachine.getTeamRed() + "");
+    }
+
+    @FXML
+    private void add1000Blue(ActionEvent actionEvent) {
+        CellMachine draw = new CellMachine(canvas);
+        draw.putRandomTypeCells(1000, 2);
+        draw.drawArray();
+        teamBlue.setText(CellMachine.getTeamBlue() + "");
     }
 
 
